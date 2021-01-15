1ee0eca92a62fd863f36c54c6a69b60161e62c78
Kiki Dawson <kikiadawson@gmail.com>
2021-01-15 17:15:24 +0000

    weather API added

--- views/interface.js
+++ views/interface.js
@@ -34,4 +34,18 @@ $(document).ready(function() {
     $('#temp').attr('class', thermostat.usage());
   }
 
+  $('#location').change(function() {
+    var location = $('#location').val();
+    displayWeather(location)
+  })
+
+  function displayWeather(location) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location
+    var token = '&appid=3014bf26bfd5415237902ed40e810f29'
+    var units = '&units=metric'
+    $.get(url, token, units, function(data) {
+      $('#temperature-outside').text(data.main.temp);
+    })
+  }
+
 })
